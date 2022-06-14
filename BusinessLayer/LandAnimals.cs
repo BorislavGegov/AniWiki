e69c08f6304e86ed0b3bf4341cfdd01b83454ba4
Borislav <borislavgegov_e18@schoolmath.eu>
2022-06-14 15:33:05 +0300

    Add project files.

---
+++ BusinessLayer/LandAnimals.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class LandAnimals
+    {
+        [Key]
+        public int ID { get; private set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Type { get; set; }
+        [Required]
+        public string Order { get; set; }
+        [Required]
+        public int Limbs { get; set; }
+        [Required]
+        public string Habitat { get; set; }
+        [Required]
+        public int Lifespan { get; set; }
+
+        private LandAnimals()
+        { }
+        public LandAnimals(string name, string type, string order, int limbs, string habitat, int lifespan)
+        {
+            Name = name;
+            Type = type;
+            Order = order;
+            Limbs = limbs;
+            Habitat = habitat;
+            Lifespan = lifespan;
+        }
+    }
+}
---
+++ BusinessLayer/WaterAnimals.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class WaterAnimals
+    {
+        [Key]
+        public int ID { get; private set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Type { get; set; }
+        [Required]
+        public string Order { get; set; }
+        [Required]
+        public int Temperature { get; set; }
+        [Required]
+        public int Depth { get; set; }
+        [Required]
+        public string Habitat { get; set; }
+        [Required]
+        public int Lifespan { get; set; }
+        private WaterAnimals()
+        { }
+        public WaterAnimals(string name, string type, string order, int temperature, int depth, string habitat, int lifespan)
+        {
+            Name = name;
+            Type = type;
+            Order = order;
+            Temperature = temperature;
+            Depth = depth;
+            Habitat = habitat;
+            Lifespan = lifespan;
+        }
+
+    }
+}
