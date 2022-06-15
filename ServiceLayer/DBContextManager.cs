60c792c9335062a4f8533fdcdad3c03eaaf3747f
Borislav <borislavgegov_e18@schoolmath.eu>
2022-06-15 13:26:40 +0300

    service layer done

---
+++ ServiceLayer/DBContextManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer;
+using DataLayer;
+
+namespace ServiceLayer
+{
+    class DBContextManager
+    {
+        private static AnimalDbContext _context;
+        private static SpeciesContext _speciesContext;
+        private static HabitatContext _habitatContext;
+        private static DietContext _dietContext;
+
+        #region DB context
+        public static void SetChangeTracking(bool value)
+        {
+            _context.ChangeTracker.AutoDetectChangesEnabled = value;
+        }
+
+        public static AnimalDbContext CreateContext()
+        {
+            _context = new AnimalDbContext();
+            return _context;
+        }
+
+        public static AnimalDbContext GetContext()
+        {
+            return _context;
+        }
+        public static SpeciesContext CreateRegionContext(AnimalDbContext context)
+        {
+            _speciesContext = new SpeciesContext(context);
+            return _speciesContext;
+        }
+
+        public static SpeciesContext GetRegionContext()
+        {
+            return _speciesContext;
+        }
+
+        public static HabitatContext CreateInterestContext(AnimalDbContext context)
+        {
+            _habitatContext = new HabitatContext(context);
+            return _habitatContext;
+        }
+
+        public static HabitatContext GetInterestContext()
+        {
+            return _habitatContext;
+        }
+        public static DietContext CreateUserContext(AnimalDbContext context)
+        {
+            _dietContext = new DietContext(context);
+            return (_dietContext);
+        }
+        public static DietContext GetUserContext()
+        {
+            return _dietContext;
+        }
+
+        #endregion
+    }
+}
---
+++ ServiceLayer/DBManager.cs
@@ -0,0 +1,82 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer
+{
+    public class DBManager<T, K>
+    {
+        IDB<T, K> context;
+
+        public DBManager(IDB<T, K> context)
+        {
+            this.context = context;
+        }
+
+        public void Create(T item)
+        {
+            try
+            {
+                context.Create(item);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public T Read(K key, bool noTracking = false, bool useNavigationProperties = false)
+        {
+            try
+            {
+                return context.Read(key);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public IEnumerable<T> ReadAll(bool useNavigationProperties = false)
+        {
+            try
+            {
+                return context.ReadAll();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Update(T item)
+        {
+            try
+            {
+                context.Update(item);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Delete(K key)
+        {
+            try
+            {
+                context.Delete(key);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+    }
+}
+
