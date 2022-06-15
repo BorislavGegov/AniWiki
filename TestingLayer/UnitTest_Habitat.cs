cd8dbf7ffad49bb7417e8961e2c5a9beb01e19d6
Borislav <borislavgegov_e18@schoolmath.eu>
2022-06-15 14:14:36 +0300

    .

---
+++ TestingLayer/UnitTest_Habitat.cs
@@ -0,0 +1,78 @@
+﻿using BusinessLayer;
+using DataLayer;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace TestingLayer
+{
+    class UnitTest_Habitat
+    {
+        private AnimalDbContext _dbContext;
+        private HabitatContext _habitatContext;
+        DbContextOptionsBuilder builder;
+
+        [SetUp]
+        public void Setup()
+        {
+            builder = new DbContextOptionsBuilder();
+            builder.UseInMemoryDatabase(Guid.NewGuid().ToString());
+
+            _dbContext = new AnimalDbContext(builder.Options);
+            _habitatContext = new HabitatContext(_dbContext);
+        }
+
+        [Test]
+        public void TestCreateGenre()
+        {
+            int habitatsBefore = _habitatContext.ReadAll().Count();
+
+            _habitatContext.Create(new Habitat("novel"));
+
+            int habitatsAfter = _habitatContext.ReadAll().Count();
+
+            Assert.IsTrue(habitatsBefore != habitatsAfter);
+        }
+
+        [Test]
+        public void TestReadGenre()
+        {
+            _habitatContext.Create(new Habitat("novel"));
+
+            Habitat genre = _habitatContext.Read(1);
+
+            Assert.That(genre != null, "There is no record with id 1!");
+        }
+
+        [Test]
+        public void TestUpdateGenre()
+        {
+            _habitatContext.Create(new Habitat("novel"));
+
+            Habitat genre = _habitatContext.Read(1);
+
+            genre.Name = "poem";
+
+            _habitatContext.Update(genre);
+
+            Habitat genre1 = _habitatContext.Read(1);
+
+            Assert.IsTrue(genre1.Name == "poem", "Habitat Update() does not change name!");
+        }
+
+        [Test]
+        public void TestDeleteGenre()
+        {
+            _habitatContext.Create(new Habitat("Delete genre"));
+
+            int habitatsBeforeDeletion = _habitatContext.ReadAll().Count();
+
+            _habitatContext.Delete(1);
+
+            int habitatsAfterDeletion = _habitatContext.ReadAll().Count();
+
+            Assert.AreNotEqual(habitatsBeforeDeletion, habitatsAfterDeletion);
+        }
+    }
+}
--- TestingLayer/UnitTest_Species.cs
+++ TestingLayer/UnitTest_Species.cs
@@ -1,9 +1,78 @@
+using BusinessLayer;
+using DataLayer;
+using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
+using System;
+using System.Linq;
 
 namespace TestingLayer
 {
     public class UnitTest_Species
     {
-        
+        private AnimalDbContext _dbContext;
+        private SpeciesContext _speciesContext;
+        DbContextOptionsBuilder builder;
+
+        [SetUp]
+        public void Setup()
+        {
+            builder = new DbContextOptionsBuilder();
+            builder.UseInMemoryDatabase(Guid.NewGuid().ToString());
+
+            _dbContext = new AnimalDbContext(builder.Options);
+            _speciesContext = new SpeciesContext(_dbContext);
+        }
+
+        [Test]
+        public void TestCreateSpecies()
+        {
+            int speciesBefore = _speciesContext.ReadAll().Count();
+
+            _speciesContext.Create(new Species("animol", "bozaenik", 13));
+
+            int speciesAfter = _speciesContext.ReadAll().Count();
+
+            Assert.IsTrue(speciesBefore != speciesAfter);
+        }
+
+        [Test]
+        public void TestReadSpecies()
+        {
+            _speciesContext.Create(new Species("animol", "bozaenik", 13));
+
+            Species species = _speciesContext.Read(1);
+
+            Assert.That(species != null, "There is no record with id 1!");
+        }
+
+        [Test]
+        public void TestUpdateSpecies()
+        {
+            _speciesContext.Create(new Species("animol", "bozaenik", 13));
+
+            Species species = _speciesContext.Read(1);
+
+            species.Name = "kotka";
+
+            _speciesContext.Update(species);
+
+            Species species1 = _speciesContext.Read(1);
+
+            Assert.IsTrue(species1.Name == "poem", "Species Update() does not change name!");
+        }
+
+        [Test]
+        public void TestDeleteSpecies()
+        {
+            _speciesContext.Create(new Species("animal", "vlechogo", 14));
+
+            int speciesBeforeDeletion = _speciesContext.ReadAll().Count();
+
+            _speciesContext.Delete(1);
+
+            int speciesAfterDeletion = _speciesContext.ReadAll().Count();
+
+            Assert.AreNotEqual(speciesBeforeDeletion, speciesAfterDeletion);
+        }
     }
 }
