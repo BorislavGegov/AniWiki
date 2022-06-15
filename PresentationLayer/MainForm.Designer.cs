4274403fa338409de0dbecf449f7572c7fa69fc9
Borislav <borislavgegov_e18@schoolmath.eu>
2022-06-15 15:02:15 +0300

    Commit 4.2

--- PresentationLayer/MainForm.Designer.cs
+++ PresentationLayer/MainForm.Designer.cs
@@ -4,12 +4,12 @@ namespace PresentationLayer
     partial class MainForm
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -24,12 +24,11 @@ namespace PresentationLayer
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -38,41 +37,38 @@ namespace PresentationLayer
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(321, 116);
+            this.button1.Location = new System.Drawing.Point(325, 117);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(94, 29);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Species";
+            this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(321, 184);
+            this.button2.Location = new System.Drawing.Point(325, 191);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(94, 29);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Habitats";
+            this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(321, 245);
+            this.button3.Location = new System.Drawing.Point(325, 256);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(94, 29);
             this.button3.TabIndex = 2;
-            this.button3.Text = "Diets";
+            this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Enabled = false;
-            this.button4.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
-            this.button4.Location = new System.Drawing.Point(321, 367);
+            this.button4.Location = new System.Drawing.Point(325, 359);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(94, 29);
             this.button4.TabIndex = 3;
-            this.button4.Text = "Exit";
+            this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // MainForm
@@ -84,9 +80,8 @@ namespace PresentationLayer
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Enabled = false;
             this.Name = "MainForm";
-            this.Text = "Form1";
+            this.Text = "MainForm";
             this.ResumeLayout(false);
 
         }
@@ -99,4 +94,3 @@ namespace PresentationLayer
         private System.Windows.Forms.Button button4;
     }
 }
-
--- PresentationLayer/MainForm.cs
+++ PresentationLayer/MainForm.cs
@@ -16,6 +16,5 @@ namespace PresentationLayer
         {
             InitializeComponent();
         }
-
     }
 }
