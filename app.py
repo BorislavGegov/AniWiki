b4cc8a7f8c04c0b889c569e9fad5460a3b09b80c
S4vyss <mlodywojownik345@gmail.com>
2023-12-01 11:51:51 +0100

    function that handles the initial collection

--- app.py
+++ app.py
@@ -4,22 +4,46 @@ from fastapi import FastAPI
 from dotenv import load_dotenv
 import json
 from pymongo import MongoClient
+import asyncio
 
 load_dotenv()
 
 app = FastAPI(
     title="Student Course API",
-    summary="A sample application showing how to use FastAPI to add a ReST API to a MongoDB collection.",
+    summary="",
 )
-client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
-db = client.get_database("buddyapi")
 
-if db.get_collection("businesses").is_mongos:
-    businesses_collection = db.get_collection("businesses")
-    print(businesses_collection)
-else:
+client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
+db = client['buddyapi']
+
+async def check_and_create_collection():
     with open('./schema.json', 'r') as file:
         schema = json.load(file)
-    businesses_collection = db.create_collection("businesses", validator={'$jsonSchema': schema})
+    
+    collection_names = await db.list_collection_names()
+    if 'businesses' in collection_names:
+        collection_info = await db.command("listCollections", filter={"name": "businesses"})
+        collection_options = collection_info['cursor']['firstBatch'][0].get('options', {})
+        
+        if 'validator' in collection_options:
+            current_schema = collection_options['validator'].get('$jsonSchema')
+            if current_schema == schema:
+                print("Collection 'businesses' already exists with the matching schema.")
+                return 0
+            else:
+                print("Collection 'businesses' already exists but with a different schema.")
+                return 1
+        else:
+            print("Collection 'businesses' does not have a validator set. Setting schema now.")
+            await db.command("collMod", "businesses", validator={'$jsonSchema': schema})
+            return 0
+    else:
+        await db.create_collection("businesses", validator={'$jsonSchema': schema})
+        print("Collection 'businesses' created with the specified schema.")
+        return 0
+
+@app.on_event("startup")
+async def startup_event():
+    await check_and_create_collection()
 
 import routes
