90349cee427f5074cd76f663a0d15a5427eacd18
Peeter100Ftr <off.piotr.teper@gmail.com>
2023-12-04 12:50:56 +0100

    function name

--- routes.py
+++ routes.py
@@ -33,13 +33,13 @@ async def search_invoices(id, date):
     return result
 
 @app.get("/business/{id}/invoices/issue_date/after/{date}")
-async def search_invoices(id, date):
+async def search_invoices_alt(id, date):
     query = await db.businesses.find({"_id": ObjectId(id)}).to_list(length=100)
     # tu sprawdzic token gdzies
     format = '%Y-%m-%d'
     converted_date = datetime.datetime.strptime(date, format)
     result = []
-    for business in query:  
+    for business in query:
         invoices = business["invoices"]
         if len(invoices) > 0:
             for invoice in invoices:
