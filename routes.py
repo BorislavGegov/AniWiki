5357fd7a252c93603c72aefcc4608d15b8252813
Peeter100Ftr <off.piotr.teper@gmail.com>
2023-12-04 12:30:06 +0100

    invoices

--- routes.py
+++ routes.py
@@ -1,5 +1,18 @@
-from app import app
+from app import app, db
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+import json
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/business/{id}/invoices")
+async def read_invoices(id):
+    query = await db.businesses.find({"_id": ObjectId(id)}).to_list(length=100)
+    # tu sprawdzic token gdzies
+    for business in query:  
+        invoice = business["invoices"]
+        if len(invoice) > 0:
+            return invoice
+    return []
