b97f355261038beca05b06986ff0f1d37faceb12
Peeter100Ftr <off.piotr.teper@gmail.com>
2023-12-05 12:43:45 +0100

    i did it - businesses, business/id, id/plots

--- routes/plots.py
+++ routes/plots.py
@@ -1,14 +1,17 @@
 from app import app, db
 from bson.objectid import ObjectId
-
-
+from fastapi import Response, status, Header
 
 @app.get("/business/{id}/plots")
-async def read_plots(id):
-    query = await db.businesses.find({"_id": ObjectId(id)}).to_list(length=100)
-    for business in query:
-        plots = business["plots"]
-        if len(plots) > 0:
-            return plots
+async def read_plots(id, token: str = Header()):
+    business = await db.businesses.find_one({"_id": ObjectId(id)})
+    if not token == business["auth_token"]:
+        return Response(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            content="Bad token"
+        )
+    plots = business["plots"]
+    if len(plots) > 0:
+        return plots
     return []
 
