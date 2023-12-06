bb0e6f4cfda71fa85760d7240f08d344b19e49c6
Michał <mdomanski.tzn@gmail.com>
2023-12-06 10:46:51 +0100

    get plot commit

--- routes/plots.py
+++ routes/plots.py
@@ -15,3 +15,17 @@ async def read_plots(id, token: str = Header()):
         return plots
     return []
 
+
+@app.get("/business/{id}/plots/{idp}")
+async def read_plots(id, idp, token: str = Header()):
+    business = await db.businesses.find_one({"_id": ObjectId(id)})
+    if not token == business["auth_token"]:
+        return Response(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            content="Bad token"
+        )
+    plots = business["plots"]
+    for plot in plots:
+        if plot["id"] == idp:
+            return plot
+    return "Plot not found"
