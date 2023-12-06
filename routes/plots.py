bc1fbdd10498325b0b7952e2eb6bdceb208388d2
Praktyki <praktyki@Mateuszs-MacBook-Air.local>
2023-12-06 11:43:44 +0100

    Add get_pitch and get_pitches

--- routes/plots.py
+++ routes/plots.py
@@ -32,3 +32,43 @@ async def read_plot_id(id, idp, token: str = Header()):
         status_code=status.HTTP_404_NOT_FOUND,
         content="Bad plot id"
     )
+
+@app.get("/business/{id}/plots/{plot_id}/pitches/{pitch_id}")
+async def get_pitch(id: str, plot_id: str, pitch_id: str, token: str = Header()):
+    
+    business = await db["businesses"].find_one({"_id": ObjectId(id)})
+
+    if not token == business["auth_token"]:
+        return Response(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            content="Bad token"
+        )
+
+    plots = business["plots"]
+    if len(plots) > 0:
+        for plot in plots:
+            if plot["id"] == plot_id:
+                pitches = plot["pitches"]
+                for pitch in pitches:
+                    if pitch["id"] == pitch_id:
+                        return pitch
+    return "Pitch not found"
+
+@app.get("/business/{id}/plots/{plot_id}/pitches")
+async def get_pitches(id: str, plot_id: str, token: str = Header()):
+    
+    business = await db["businesses"].find_one({"_id": ObjectId(id)})
+
+    if not token == business["auth_token"]:
+        return Response(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            content="Bad token"
+        )
+    
+    
+    plots = business["plots"]
+    if len(plots) > 0:
+        for plot in plots:
+            if plot["id"] == plot_id:
+                return plot["pitches"]
+    return "Bad plot id"
